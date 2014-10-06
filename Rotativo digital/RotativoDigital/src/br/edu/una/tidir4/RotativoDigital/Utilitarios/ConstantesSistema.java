423bcf5269572569974604cc5c37f027105968d2
Geraldo dos Anjos <geraldo_dos_anjos@hotmail.com>
2014-10-06 10:45:49 -0300

    Primeiro envio
    Esqueleto para web

---
+++ Rotativo digital/RotativoDigital/src/br/edu/una/tidir4/RotativoDigital/Utilitarios/ConstantesSistema.java
@@ -0,0 +1,13 @@
+package br.edu.una.tidir4.RotativoDigital.Utilitarios;
+
+public class ConstantesSistema {
+	private String nomeSistema ="Sistema de Controle de Estacionamento Rotativo Digital";
+
+	public String getNomeSistema() {
+		return nomeSistema;
+	}
+
+	public void setNomeSistema(String nomeSistema) {
+		this.nomeSistema = nomeSistema;
+	}
+}
